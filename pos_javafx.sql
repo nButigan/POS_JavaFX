2b0920cd31fe2b69ad626744da9405a20aeabe28
nButigan <45075242+nButigan@users.noreply.github.com>
2020-03-14 15:35:54 +0100

    baza

---
+++ pos_javafx.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2020 at 03:34 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos_javafx`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `korisnik`
+--
+
+CREATE TABLE `korisnik` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `prezime` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `korisnickoIme` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `uloga` tinyint(4) NOT NULL DEFAULT '0',
+  `sifra` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+INSERT INTO `korisnik` (`id`, `ime`, `prezime`, `korisnickoIme`, `uloga`, `sifra`) VALUES
+(1, 'nikola', 'butigan', 'nbutigan', 0, '12345'),
+(3, '', '', 'admin', 1, '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `korisnickoIme` (`korisnickoIme`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
