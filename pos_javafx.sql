443b735633f9d16ef380d93af07db3b15ac5bd1d
nButigan <45075242+nButigan@users.noreply.github.com>
2020-03-16 19:29:26 +0100

    Add files via upload

--- pos_javafx.sql
+++ pos_javafx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2020 at 03:34 PM
+-- Generation Time: Mar 16, 2020 at 07:28 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -21,6 +21,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `pos_javafx`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikl`
+--
+
+CREATE TABLE `artikl` (
+  `id` int(11) NOT NULL,
+  `naziv` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `dobavljac` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `cijena` float NOT NULL,
+  `slika` blob NOT NULL,
+  `kategorija` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikl_racun`
+--
+
+CREATE TABLE `artikl_racun` (
+  `racun_fk` int(11) NOT NULL,
+  `artikl_fk` int(11) NOT NULL,
+  `kolicina` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -45,9 +72,47 @@ INSERT INTO `korisnik` (`id`, `ime`, `prezime`, `korisnickoIme`, `uloga`, `sifra
 (1, 'nikola', 'butigan', 'nbutigan', 0, '12345'),
 (3, '', '', 'admin', 1, '12345');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `racun`
+--
+
+CREATE TABLE `racun` (
+  `id` int(11) NOT NULL,
+  `datum` date NOT NULL,
+  `korisnik_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skladiste`
+--
+
+CREATE TABLE `skladiste` (
+  `id` int(11) NOT NULL,
+  `artikl_fk` int(11) NOT NULL,
+  `kolicina` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `artikl`
+--
+ALTER TABLE `artikl`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `naziv` (`naziv`);
+
+--
+-- Indexes for table `artikl_racun`
+--
+ALTER TABLE `artikl_racun`
+  ADD KEY `artikl_fk` (`artikl_fk`),
+  ADD KEY `racun_fk` (`racun_fk`);
 
 --
 -- Indexes for table `korisnik`
@@ -57,14 +122,69 @@ ALTER TABLE `korisnik`
   ADD UNIQUE KEY `korisnickoIme` (`korisnickoIme`);
 
 --
+-- Indexes for table `racun`
+--
+ALTER TABLE `racun`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `korisnik_fk` (`korisnik_fk`);
+
+--
+-- Indexes for table `skladiste`
+--
+ALTER TABLE `skladiste`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artikl_fk` (`artikl_fk`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `artikl`
+--
+ALTER TABLE `artikl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `korisnik`
 --
 ALTER TABLE `korisnik`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `racun`
+--
+ALTER TABLE `racun`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `skladiste`
+--
+ALTER TABLE `skladiste`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `artikl_racun`
+--
+ALTER TABLE `artikl_racun`
+  ADD CONSTRAINT `artikl_racun_ibfk_1` FOREIGN KEY (`artikl_fk`) REFERENCES `artikl` (`id`),
+  ADD CONSTRAINT `artikl_racun_ibfk_2` FOREIGN KEY (`racun_fk`) REFERENCES `racun` (`id`);
+
+--
+-- Constraints for table `racun`
+--
+ALTER TABLE `racun`
+  ADD CONSTRAINT `racun_ibfk_1` FOREIGN KEY (`korisnik_fk`) REFERENCES `korisnik` (`id`);
+
+--
+-- Constraints for table `skladiste`
+--
+ALTER TABLE `skladiste`
+  ADD CONSTRAINT `skladiste_ibfk_1` FOREIGN KEY (`artikl_fk`) REFERENCES `artikl` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
